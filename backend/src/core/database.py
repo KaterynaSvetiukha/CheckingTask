aba177bdde1c67a85c551e2f50f0203320fa09cd
Kateryna <katyasvetyha@gmail.com>
2026-08-22 13:30:34 +0300

    fix: add dotenv for safety

--- backend/src/core/database.py
+++ backend/src/core/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
+from src.core.config import setting
 
-DATABASE_URL = "postgresql+asyncpg://postgres:admin@localhost:5432/checking_task"
-
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(setting.DATABASE_URL_asyncpg, echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 Base = declarative_base()
